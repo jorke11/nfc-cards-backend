df39ac7b51fe8104f709e94762de7e93a19efaee
Jorge pinedo <jpinedom@hotmail.com>
2026-03-06 13:53:22 -0500

    change port

--- src/config/gmail.config.ts
+++ src/config/gmail.config.ts
@@ -4,6 +4,6 @@ export default registerAs('gmail', () => ({
   user: process.env.GMAIL_USER,
   password: process.env.GMAIL_PASSWORD,
   host: 'smtp.gmail.com',
-  port: 587,
-  secure: false, // true for 465, false for other ports
+  port: 465,
+  secure: true, // SSL/TLS
 }));
