43c7e05b0786172aca455d8157fc36cff0b362d7
Jorge pinedo <jpinedom@hotmail.com>
2026-03-08 15:59:42 -0500

    change sysc

--- src/migrate.ts
+++ src/migrate.ts
@@ -13,7 +13,7 @@ const AppDataSource = new DataSource({
   database: process.env.DATABASE_NAME,
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   migrations: [__dirname + '/migrations/**/{.ts,.js}'],
-  synchronize: true,
+  synchronize: false,
   logging: true,
 });
 
